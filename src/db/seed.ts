bd19319015e71263bcc32e95ddc3b1b64d535678
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-10-21 11:21:23 -0300

    feat: alter db schema

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -3,25 +3,39 @@ import { client, db } from '.'
 import { goalCompletions, goals } from './schema'
 
 async function seed() {
-  await db.delete(goalCompletions)
-  await db.delete(goals)
+  try {
+    // Iniciar uma transação para garantir a consistência
+    await db.transaction(async trx => {
+      // Deletar completions antes de goals devido à dependência
+      await trx.delete(goalCompletions)
+      await trx.delete(goals)
 
-  const result = await db
-    .insert(goals)
-    .values([
-      { title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
-      { title: 'Fazer exercícios', desiredWeeklyFrequency: 3 },
-      { title: 'Estudar inglês', desiredWeeklyFrequency: 7 },
-    ])
-    .returning()
+      // Inserir novos dados na tabela goals
+      const result = await trx
+        .insert(goals)
+        .values([
+          { title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
+          { title: 'Fazer exercícios', desiredWeeklyFrequency: 3 },
+          { title: 'Estudar inglês', desiredWeeklyFrequency: 7 },
+        ])
+        .returning()
 
-  const startoOfWeek = dayjs().startOf('week')
+      // Calcular o início da semana
+      const startOfWeek = dayjs().startOf('week')
 
-  await db.insert(goalCompletions).values([
-    { goalId: result[0].id, createdAt: startoOfWeek.toDate() },
-    { goalId: result[1].id, createdAt: startoOfWeek.add(1, 'day').toDate() },
-    { goalId: result[2].id, createdAt: startoOfWeek.add(3, 'day').toDate() },
-  ])
+      // Inserir os completions relacionados às metas criadas
+      await trx.insert(goalCompletions).values([
+        { goalId: result[0].id, createdAt: startOfWeek.toDate() },
+        { goalId: result[1].id, createdAt: startOfWeek.add(1, 'day').toDate() },
+        { goalId: result[2].id, createdAt: startOfWeek.add(3, 'day').toDate() },
+      ])
+    })
+  } catch (error) {
+    console.error('Erro ao fazer o seed:', error)
+  } finally {
+    // Fechar a conexão com o banco de dados
+    client.end()
+  }
 }
 
-seed().finally(() => client.end())
+seed()
