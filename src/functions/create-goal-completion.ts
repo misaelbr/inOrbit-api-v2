314c0b745f47d68d38e2a9fb22f3ee270b53eb30
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-10-24 12:04:14 -0300

    feat: add support to gamification

--- src/functions/create-goal-completion.ts
+++ src/functions/create-goal-completion.ts
@@ -1,6 +1,6 @@
-import { and, gte, lte, count, eq, sql } from 'drizzle-orm'
+import { and, gte, lte, count, eq, sql, is } from 'drizzle-orm'
 import { db } from '../db'
-import { goalCompletions, goals } from '../db/schema'
+import { goalCompletions, goals, users } from '../db/schema'
 import dayjs from 'dayjs'
 
 interface CreateGoalCompletionRequest {
@@ -50,17 +50,38 @@ export async function createGoalCompletion({
   const { completionCount, desiredWeeklyFrequency } = result[0]
 
   if (completionCount >= desiredWeeklyFrequency) {
-    throw new Error('Goal already completed')
+    throw new Error('Goal already completed this week')
   }
 
-  const insertResult = await db
-    .insert(goalCompletions)
-    .values({
-      goalId,
-    })
-    .returning()
+  const isLastCompletionFromGoal =
+    completionCount + 1 === desiredWeeklyFrequency
 
-  const goalCompletion = insertResult[0]
+  const earnedExperience = isLastCompletionFromGoal ? 7 : 5
+
+  const goalCompletion = await db.transaction(async tx => {
+    const [goalCompletion] = await db
+      .insert(goalCompletions)
+      .values({
+        goalId,
+      })
+      .returning()
+
+    await db
+      .update(users)
+      .set({
+        experience: sql`${users.experience} + ${earnedExperience}`,
+      })
+      .where(eq(users.id, userId))
+
+    return goalCompletion
+  })
+
+  // const insertResult = await db
+  //   .insert(goalCompletions)
+  //   .values({
+  //     goalId,
+  //   })
+  //   .returning()
 
   return {
     goalCompletion,
